b1a85ee7ee138c1b449712690a75bc537c47969b
omarquezada1 <64980731+omarquezada1@users.noreply.github.com>
2020-06-23 10:36:09 -0500

    Reparacion de errorres

--- server/models/producto.js
+++ server/models/producto.js
@@ -1,53 +1,59 @@
-const mongoose = require('mongoose');
-const uniquevalidator = require('mongoose-unique-validator');
+const mongoose = require("mongoose");
+const uniquevalidator = require("mongoose-unique-validator");
 
+const Negocio = require("./negocio");
 let Schema = mongoose.Schema;
 
 let productoSchema = new Schema({
-
-    marca: {
-        type: String,
-        required: [true, 'ingresa la marca']
+  marca: {
+    type: String,
+    required: [true, "ingresa la marca"],
+  },
+  modelo: {
+    type: String,
+    required: [true, " ingresa el modelo"],
+  },
+  nombre: {
+    type: String,
+    required: [true, " ingresa el nombre"],
+  },
+  cdb: {
+    type: String,
+    required: [true, " ingresa el codigo de barra"],
+  },
+  tiendas: [
+    {
+      required: false,
+      _id: 0,
+      precio: Number,
+      oferta: { type: Boolean },
+      inventario: Number,
+      negocio: {
+        type: Schema.Types.ObjectId,
+        ref: "Negocio",
+      },
     },
-    modelo: {
-        type: String,
-        required: [true, ' ingresa el modelo']
-    },
-    nombre: {
-        type: String,
-        required: [true, ' ingresa el nombre']
-    },
-    cdb: {
-        type: String,
-        required: [true, ' ingresa el codigo de barra']
-    },
-    precio: {
-        type: String,
-        required: [true, ' ingresa el precio']
-    },
-    descripcion: {
-        type: String,
-        required: [true, 'Por favor ingresa la descripcion']
-    },
-    alias: {
-        type: String,
-        required: [true, 'Por favor ingresa el alias']
-    },
-    img: {
-        type: String,
-        default: 'noImage.jpeg'
-    },
-    estatus: {
-        type: Boolean,
-        default: true
-    }
-
-
-
+  ],
+  descripcion: {
+    type: String,
+    required: [true, "Por favor ingresa la descripcion"],
+  },
+  alias: {
+    type: String,
+    required: [true, "Por favor ingresa el alias"],
+  },
+  img: {
+    type: String,
+    default: "noImage.jpeg",
+  },
+  estatus: {
+    type: Boolean,
+    default: true,
+  },
 });
 
 productoSchema.plugin(uniquevalidator, {
-    message: '{PATH} Debe ser unico y diferente'
+  message: "{PATH} Debe ser unico y diferente",
 });
 
-module.exports = mongoose.model('Producto', productoSchema);
+module.exports = mongoose.model("Producto", productoSchema);
--- server/routes/usuario/buscar.js
+++ server/routes/usuario/buscar.js
@@ -4,21 +4,18 @@ const Usuario = require("../../models/usuario");
 const app = express();
 
 app.get("/buscar/:usuario", (req, res) => {
-    let usuario = req.params.usuario;
-    Usuario.find(
-        {username:usuario},
-      (err, usrDB) => {
-        if (err) {
-          return res.status(400).json({
-            ok: false,
-            err
-          });
-        }
-        return res.status(200).json({
-          ok: true,
-          cont: usrDB
-        });
-      }
-    );
+  let usuario = req.params.usuario;
+  Usuario.find({ username: usuario }, (err, usrDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    return res.status(200).json({
+      ok: true,
+      cont: usrDB,
+    });
   });
-  module.exports = app;
+});
+module.exports = app;
