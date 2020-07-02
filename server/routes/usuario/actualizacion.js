1c38862e2b82513ecda507e29a85cf007c5cff8a
omarquezada1 <64980731+omarquezada1@users.noreply.github.com>
2020-07-02 15:12:44 -0500

    Ultima Version

--- server/routes/usuario/actualizacion.js
+++ server/routes/usuario/actualizacion.js
@@ -1,29 +1,67 @@
 const express = require("express");
 const _ = require("underscore");
 const Usuario = require("../../models/usuario");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.put("/actualizar/:id", (req, res) => {
-    let id = req.params.id;
-    let body = _.pick(req.body, ["nombre", "apellidos", "contraseña", "telefono", "img"]);
-  
-    Usuario.findByIdAndUpdate(
-      id,
-      body,
-      { new: true, runValidators: true, context: "query" },
-      (err, usrDB) => {
-        if (err) {
-          return res.status(400).json({
-            ok: false,
-            err
-          });
-        }
-        return res.status(200).json({
-          ok: true,
-          msg: `Usuario actualizado correctamente`,
-          cont: usrDB
+  let id = req.params.id;
+  let body = _.pick(req.body, [
+    "nombre",
+    "apellidos",
+    "contraseña",
+    "telefono",
+    "img",
+  ]);
+
+  Usuario.findByIdAndUpdate(
+    id,
+    body,
+    { new: true, runValidators: true, context: "query" },
+    (err, usrDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
         });
       }
-    );
-  });
-  module.exports = app;
+      return res.status(200).json({
+        ok: true,
+        msg: `Usuario actualizado correctamente`,
+        cont: usrDB,
+      });
+    }
+  );
+});
+
+app.put("/carrito/:idUsuario", (req, res) => {
+  let id = req.params.idUsuario;
+  let body = req.body;
+  Usuario.findByIdAndUpdate(
+    id,
+    {
+      $push: {
+        carrito: {
+          cantidad: body.cantidad,
+          negocio: body.negocio,
+          producto: body.producto,
+        },
+      },
+    },
+    { new: true, runValidators: true, context: "query" },
+    (err, usrDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+      return res.status(200).json({
+        ok: true,
+        msg: `Usuario actualizado correctamente`,
+        cont: usrDB,
+      });
+    }
+  );
+});
+module.exports = app;
