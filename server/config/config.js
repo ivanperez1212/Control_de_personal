ed2e0f052961389dcfa768654eac366b0461e89a
omarquezada1 <64980731+omarquezada1@users.noreply.github.com>
2020-07-06 17:59:18 -0500

    make it better

--- server/config/config.js
+++ server/config/config.js
@@ -3,21 +3,6 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
 
-//entorno
-process.env.NODE_ENV = process.env.NODE_ENV || "dev";
-
-//conexion a la db
-let urlDB;
-
-if (process.env.NODE_ENV === "dev") {
-    urlDB = "mongodb+srv://zaza1:OM945kWYtSdBNQCY@cluster0-21svm.mongodb.net/test?retryWrites=true&w=majority";
-
-} else {
-    urlDB = "mongodb://localhost:27017/ZAZA";
-}
-
-process.env.URLDB = urlDB;
-
 //firma de JWT
 process.env.SEED = process.env.SEED || "firma-super-secreta";
 
