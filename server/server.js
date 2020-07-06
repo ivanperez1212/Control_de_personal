168b0e69fb550890bdb28a63c61da06f34c2c823
omarquezada1 <64980731+omarquezada1@users.noreply.github.com>
2020-07-06 18:24:50 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -3,20 +3,10 @@ require("./config/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+var cors = require("cors");
 const app = express();
 //habilita CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
+app.use(cors({ origin: true, credentials: true }));
 
 //parse application/x-www-from-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
