0d2171906077a5444942e50a1421edb6134b77d8
omarquezada1 <64980731+omarquezada1@users.noreply.github.com>
2020-07-06 18:51:07 -0500

    make it better

--- server/routes/usuario/actualizacion.js
+++ server/routes/usuario/actualizacion.js
@@ -4,26 +4,124 @@ const Usuario = require("../../models/usuario");
 const app = express();
 
 app.put("/actualizar/:id", (req, res) => {
-    let id = req.params.id;
-    let body = _.pick(req.body, ["nombre", "apellidos", "contraseña", "telefono", "img"]);
-  
-    Usuario.findByIdAndUpdate(
-      id,
-      body,
-      { new: true, runValidators: true, context: "query" },
-      (err, usrDB) => {
-        if (err) {
-          return res.status(400).json({
-            ok: false,
-            err
-          });
-        }
-        return res.status(200).json({
-          ok: true,
-          msg: `Usuario actualizado correctamente`,
-          cont: usrDB
+  let id = req.params.id;
+  let body = _.pick(req.body, [
+    "nombre",
+    "apellidos",
+    "contraseña",
+    "telefono",
+    "img",
+  ]);
+
+  Usuario.findByIdAndUpdate(
+    id,
+    body,
+    { new: true, runValidators: true, context: "query" },
+    (err, usrDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
         });
       }
-    );
+      return res.status(200).json({
+        ok: true,
+        msg: `Usuario actualizado correctamente`,
+        cont: usrDB,
+      });
+    }
+  );
+});
+app.put("/carrito/:idUsuario", (req, res) => {
+  let id = req.params.idUsuario;
+  let body = req.body;
+  Usuario.findByIdAndUpdate(
+    id,
+    {
+      $push: {
+        carrito: {
+          cantidad: body.cantidad,
+          precio: body.precio,
+          negocio: body.negocio,
+          producto: body.producto,
+        },
+      },
+    },
+    { new: true, runValidators: true, context: "query" },
+    (err, usrDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+      return res.status(200).json({
+        ok: true,
+        msg: `Usuario actualizado correctamente`,
+        cont: usrDB,
+      });
+    }
+  );
+});
+
+app.get("/actualizarUsuario/:idUsuario", (req, res) => {
+  let id = req.params.idUsuario;
+  Usuario.findById(id, (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+    Negocio.populate(usuarioDB, { path: "carrito.negocio" }, function (
+      err,
+      usuarioDB
+    ) {
+      Producto.populate(usuarioDB, { path: "carrito.producto" }, function (
+        err,
+        usuarioDB
+      ) {
+        return res.status(200).json({
+          ok: true,
+          resp: usuarioDB,
+          // token
+        });
+      });
+    });
   });
-  module.exports = app;
+});
+
+app.put("/eliminarCarrito/:idUsuario", (req, res) => {
+  let id = req.params.idUsuario;
+  let body = req.body;
+  Usuario.findByIdAndUpdate(
+    id,
+    { $pull: { carrito: { _id: body.idCarrito } } },
+    { new: true, runValidators: true, context: "query" },
+    (err, usuarioDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
+
+      Negocio.populate(usuarioDB, { path: "carrito.negocio" }, function (
+        err,
+        usuarioDB
+      ) {
+        Producto.populate(usuarioDB, { path: "carrito.producto" }, function (
+          err,
+          usuarioDB
+        ) {
+          return res.status(200).json({
+            ok: true,
+            cont: usuarioDB,
+            // token
+          });
+        });
+      });
+    }
+  );
+});
+module.exports = app;
