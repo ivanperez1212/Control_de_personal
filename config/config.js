33aa50f6c1eae5c2bdf1029b8cee50b8342609f1
ivan <ivanperezjimenez1l40@outlook.es>
2022-04-01 15:11:18 -0600

    make it better

--- config/config.js
+++ config/config.js
@@ -13,12 +13,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 //conexion a la db
 let urlDB;
 
-if (process.env.NODE_ENV === "dev") {
+
   console.log(process.env.NAMEDB)
     urlDB= `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.plo5k.mongodb.net/${process.env.NAMEDB}?retryWrites=true&w=majority`
   // urlDB = "mongodb://localhost:27017/Control";
 
-} 
 
  
 process.env.URLDB = urlDB;
