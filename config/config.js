4ab3595e34459c93c64124c6cf46a6f25206928d
ivan <ivanperezjimenez1l40@outlook.es>
2022-04-01 19:53:15 -0600

    se subio a heroku

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 /* jshint esversion: 8 */
-
+require('dotenv').config()
 //multer host de imagenes
  let host
 
 
 //PUERTO
-process.env.PORT = process.env.PORT || 3001;
+process.env.PORT = process.env.PORT || 3000;
 
 //entorno
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
@@ -14,8 +14,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 let urlDB;
 
 if (process.env.NODE_ENV === "dev") {
-  //  urlDB= "mongodb+srv://ivan1:<ivan123>@cluster0.plo5k.mongodb.net/Control?retryWrites=true&w=majority"
-  urlDB = "mongodb://localhost:27017/Control";
+   urlDB= "mongodb+srv://ivan1:Vs7NhnR9GcgoFD2l@cluster0.plo5k.mongodb.net/Control?retryWrites=true&w=majority"
+  // urlDB = "mongodb://localhost:27017/Control";
 
 } 
 
